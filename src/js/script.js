59b56296212433472834d41283c5bca74baa5f3d
Denis <dyax17177@gmail.com>
2021-03-08 15:04:37 +0300

    made slider and adaptatoun

---
+++ src/js/script.js
@@ -0,0 +1,25 @@
+// slider
+
+const slider = tns({
+  container: '.carousel__inner',
+  items: 1,
+  slideBy: 'page',
+  autoplay: false,
+  controls: false,
+  nav: true,
+  navPosition: 'bottom',  
+  responsive: {
+    575: {
+      nav: false
+    }
+  }
+});
+
+
+document.querySelector('.prev').addEventListener('click', function () {
+  slider.goTo('prev');
+});
+
+document.querySelector('.next').addEventListener('click', function () {
+  slider.goTo('next');
+});
